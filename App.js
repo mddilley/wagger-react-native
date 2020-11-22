7df26a9d4149f3e5a272df586ef2f57c95abe378
Michael Dilley <mike.dilley@gmail.com>
2020-11-22 17:35:51 -0600

    Simplify Amplify setup with Authenticator wrapper

--- App.js
+++ App.js
@@ -1,12 +1,44 @@
 import React from "react";
 import { ScrollView, StatusBar } from "react-native";
-import Login from "./components/Login/Login";
+import Amplify from "aws-amplify";
+import { Authenticator } from "aws-amplify-react-native";
+import awsmobile from "./aws-exports";
 
-export default function App() {
+Amplify.configure({
+  ...awsmobile,
+  Analytics: {
+    disabled: true,
+  },
+});
+
+const signUpConfig = {
+  hideAllDefaults: true,
+  signUpFields: [
+    {
+      label: "Email",
+      key: "email",
+      required: true,
+      displayOrder: 1,
+      type: "string",
+    },
+    {
+      label: "Password",
+      key: "password",
+      required: true,
+      displayOrder: 2,
+      type: "password",
+    },
+  ],
+};
+
+const App = () => {
   return (
-    <ScrollView>
-      <StatusBar barStyle="dark-content" />
-      <Login />
-    </ScrollView>
+    <Authenticator usernameAttributes="email" signUpConfig={signUpConfig}>
+      <ScrollView>
+        <StatusBar barStyle="dark-content" />
+      </ScrollView>
+    </Authenticator>
   );
-}
+};
+
+export default App;
