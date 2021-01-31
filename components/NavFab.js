1bd2999f7f318b7878f0f85907a794acb6806574
Michael Dilley <mike.dilley@gmail.com>
2021-01-30 19:29:16 -0600

    Map through routeConfigs entries to create NavFab buttons

--- components/NavFab.js
+++ components/NavFab.js
@@ -2,36 +2,44 @@ import React from "react";
 import { Actions } from "react-native-router-flux";
 import { Button, Fab, Icon, Text } from "react-native-magnus";
 import { textColors } from "../styles/colors";
+import { routesConfig } from "../routes/routes";
 
 const appHeaderStyles = {
   iconSize: "2xl",
 };
 
 const NavFab = () => {
-  const handleNavPress = () => Actions.allPlaydates();
+  const handleNavPress = (key) => Actions[key]();
+  const fabRouteConfigs = Object.entries(routesConfig).reduce(
+    (acc, [key, config]) => (config.isFab ? { ...acc, [key]: config } : acc),
+    {}
+  );
 
   return (
     <Fab bg={textColors.header} h={50} w={50}>
-      <Button
-        mt="lg"
-        px="xl"
-        py="lg"
-        bg={textColors.header}
-        color="white"
-        suffix={
-          <Icon
-            name="arrowright"
-            ml="md"
-            fontSize={appHeaderStyles.iconSize}
-            color="white"
-          />
-        }
-        onPress={handleNavPress}
-      >
-        <Text fontSize="xl" color="white" fontWeight="bold">
-          Find a Playdate 😊
-        </Text>
-      </Button>
+      {Object.entries(fabRouteConfigs).map(([key, config]) => (
+        <Button
+          key={key}
+          mt="lg"
+          px="xl"
+          py="lg"
+          bg={textColors.header}
+          color="white"
+          suffix={
+            <Icon
+              name="arrowright"
+              ml="md"
+              fontSize={appHeaderStyles.iconSize}
+              color="white"
+            />
+          }
+          onPress={() => handleNavPress(key)}
+        >
+          <Text fontSize="xl" color="white" fontWeight="bold">
+            {config.navText}
+          </Text>
+        </Button>
+      ))}
     </Fab>
   );
 };
