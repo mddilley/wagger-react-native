200260fa99eb9b89a1eefe6db015e044d5b1f4c2
Michael Dilley <mike.dilley@gmail.com>
2021-01-30 19:14:35 -0600

    Update routesConfig with navText and isFab

--- routes/routes.js
+++ routes/routes.js
@@ -6,19 +6,46 @@ import MyPlaydates from "../screens/MyPlaydates";
 import PastPlaydates from "../screens/PastPlaydates";
 import Profile from "../screens/Profile";
 
-export const routeConfigs = {
-  wagger: { title: "Wagger", component: Home, initial: true },
-  myPlaydates: { title: "My Playdates", component: MyPlaydates },
-  allPlaydates: { title: "Playdates", component: AllPlaydates },
-  pastPlaydates: { title: "Past Playdates", component: PastPlaydates },
-  profile: { title: "Profile", component: Profile },
+export const routesConfig = {
+  wagger: {
+    title: "Wagger",
+    component: Home,
+    initial: true,
+    navText: "Home",
+    isFab: true,
+  },
+  myPlaydates: {
+    title: "My Playdates",
+    component: MyPlaydates,
+    navText: "Planned playdates",
+    isFab: true,
+  },
+  allPlaydates: {
+    title: "Playdates",
+    component: AllPlaydates,
+    navText: "Find a playdate",
+    isFab: true,
+  },
+  pastPlaydates: {
+    title: "Past Playdates",
+    component: PastPlaydates,
+    navText: "Past playdates",
+    isFab: true,
+  },
+  profile: {
+    title: "Profile",
+    component: Profile,
+    navText: null,
+    isFab: false,
+  },
 };
 
 const Routes = () => (
   <Router>
     <Stack hideNavBar key="root">
-      {Object.entries(routeConfigs).map(([key, config]) => {
+      {Object.entries(routesConfig).map(([key, config]) => {
         const { component, initial = false } = config;
+
         return <Scene key={key} component={component} intial={initial} />;
       })}
     </Stack>
