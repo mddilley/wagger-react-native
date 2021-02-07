a93926fde0a792685d814d0d43fe708d1ec68cae
Michael Dilley <mike.dilley@gmail.com>
2021-02-07 15:38:51 -0600

    Get sign up lambda endpoint from environment, get email from lambda event

--- events/addUserToDBpreSignUp.py
+++ events/addUserToDBpreSignUp.py
@@ -1,11 +1,22 @@
 import requests
 import json
+import os
 
 insert_user = """mutation ($email: Text) {
-  insert_Users(objects: {email: $email}) {
-    affected_rows
-  }
-}"""
+    insert_Users(objects: {email: $email}) {
+        affected_rows
+    }
+    }"""
 
-url = ''
-r = requests.post(url, json={'query': insert_user, "variables": })
+url = os.environ.get("API_ENDPOINT")
+
+def lambda_handler(event, context):
+    # Get user's email
+    email = event['request']['userAttributes']['email']
+
+    # Add them to the DB
+    try:
+        r = requests.post(url, json={'query': insert_user, "variables": { email: email } })
+        r.raise_for_status()
+    except requests.HTTPError as exception:
+        print(exception)
