998f5421e6d28725c7f63473bf49a6c4a8dc387e
Michael Dilley <mike.dilley@gmail.com>
2021-02-07 15:49:54 -0600

    Add pre signup lambda request headers and admin secret env var

--- events/addUserToDBpreSignUp.py
+++ events/addUserToDBpreSignUp.py
@@ -2,21 +2,33 @@ import requests
 import json
 import os
 
-insert_user = """mutation ($email: Text) {
+insert_user = """mutation ($email: Text!) {
     insert_Users(objects: {email: $email}) {
         affected_rows
     }
-    }"""
+}"""
 
-url = os.environ.get("API_ENDPOINT")
+ADMIN_SECRET = os.environ["ADMIN_SECRET"]
+URL = os.environ.get("API_ENDPOINT")
+HEADERS = {
+    "Content-Type": "application/json",
+    "X-Hasura-Admin-Secret": ADMIN_SECRET,
+}
+
 
 def lambda_handler(event, context):
     # Get user's email
-    email = event['request']['userAttributes']['email']
+    email = event["request"]["userAttributes"]["email"]
 
     # Add them to the DB
     try:
-        r = requests.post(url, json={'query': insert_user, "variables": { email: email } })
+        r = requests.post(
+            URL,
+            json={"query": insert_user, "variables": {email: email}},
+            headers=HEADERS,
+        )
         r.raise_for_status()
+
+        return {"statusCode": 200, "body": json.dumps({"message": "success"})}
     except requests.HTTPError as exception:
         print(exception)
