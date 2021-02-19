45b73225c33c5afbd03c8b7106087b3872a8d49d
Michael Dilley <mike.dilley@gmail.com>
2021-02-18 19:48:14 -0600

    Init Apollo Client and setup endpoint and auth headers

--- app/App.js
+++ app/App.js
@@ -8,10 +8,15 @@ import { theme } from "./styles/theme";
 
 import Amplify from "aws-amplify";
 import { Authenticator } from "aws-amplify-react-native";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import CustomLogin from "./auth/CustomLogin";
 import awsmobile from "./aws-exports";
 import { useUserSession } from "./auth/userSession";
 
+const GRAPHQL_ENDPOINT =
+  "https://wagger-react-native-db.herokuapp.com/v1/graphql";
+
+// Configure AWS Amplify
 Amplify.configure({
   ...awsmobile,
   Analytics: {
@@ -46,6 +51,30 @@ export const FlexSafeAreaView = styled(SafeAreaView)`
 const App = () => {
   const { user } = useUserSession();
 
+  // Setup Apollo Links to connect to endpoint and add headers
+  const httpLink = createHttpLink({ uri: GRAPHQL_ENDPOINT });
+
+  const authLink = setContext((_, { headers }) => {
+    // Get the authentication token and role from user if it exists
+    const token = getJwt(user);
+    const role = getHighestRole(user);
+
+    // Return the headers and role to the context so httpLink can read them
+    return {
+      headers: {
+        ...headers,
+        authorization: token ? `Bearer ${token}` : "",
+        "x-hasura-role": role ? role : "",
+      },
+    };
+  });
+
+  // Initialize Apollo Client
+  const client = new ApolloClient({
+    link: authLink.concat(httpLink),
+    cache: new InMemoryCache(),
+  });
+
   return (
     <ThemeProvider theme={theme}>
       <StatusBar barStyle={"dark-content"} />
@@ -59,10 +88,10 @@ const App = () => {
             <CustomLogin />
           </Authenticator>
         ) : (
-          <>
+          <ApolloProvider client={client}>
             <Routes />
             <NavFab />
-          </>
+          </ApolloProvider>
         )}
       </FlexSafeAreaView>
     </ThemeProvider>
