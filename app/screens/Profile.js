35ae7ba9aff1beaccb525eefb4b1f0b17870b4d7
Michael Dilley <mike.dilley@gmail.com>
2021-02-18 19:28:31 -0600

    Get user JWT with roles injected through Cognito pre token trigger

--- app/screens/Profile.js
+++ app/screens/Profile.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Auth } from "aws-amplify";
-import { Button, Div, Icon } from "react-native-magnus";
+import { useUserSession } from "../auth/userSession";
+import { Button, Div, Icon, Text } from "react-native-magnus";
 import { colors } from "../styles/colors";
 
 const signOut = async () => {
@@ -11,29 +12,35 @@ const signOut = async () => {
   }
 };
 
-const Preferences = () => (
-  <Div m="lg" p="xl">
-    <Button
-      bg={colors.danger}
-      block
-      py="lg"
-      my="xl"
-      px="lg"
-      fontSize="lg"
-      onPress={signOut}
-      suffix={
-        <Icon
-          ml="md"
-          fontFamily="MaterialCommunityIcons"
-          name="logout"
-          fontSize="3xl"
-          color={colors.white}
-        />
-      }
-    >
-      Sign out
-    </Button>
-  </Div>
-);
+const Preferences = () => {
+  const { user } = useUserSession();
+
+  console.log(user?.signInUserSession?.idToken?.jwtToken);
+
+  return (
+    <Div m="lg" p="xl">
+      <Button
+        bg={colors.danger}
+        block
+        py="lg"
+        my="xl"
+        px="lg"
+        fontSize="lg"
+        onPress={signOut}
+        suffix={
+          <Icon
+            ml="md"
+            fontFamily="MaterialCommunityIcons"
+            name="logout"
+            fontSize="3xl"
+            color={colors.white}
+          />
+        }
+      >
+        Sign out
+      </Button>
+    </Div>
+  );
+};
 
 export default Preferences;
